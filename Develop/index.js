f21a9957ddac2c924aa68223313b7e34b8dd0d5c
mheyn95 <mheyn95@gmail.com>
2021-01-16 18:16:51 -0700

    small change to naming readme file

--- Develop/index.js
+++ Develop/index.js
@@ -126,20 +126,24 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(answers) {
   return new Promise((resolve, reject) => {
-    fs.writeFile(`./README.md`, generateMarkdown(answers), (err) => {
-      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-      if (err) {
-        reject(err);
-        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-        return;
-      }
+    fs.writeFile(
+      `./${answers.title}_README.md`,
+      generateMarkdown(answers),
+      (err) => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+          reject(err);
+          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+          return;
+        }
 
-      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-      resolve({
-        ok: true,
-        message: "File created!",
-      });
-    });
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+          ok: true,
+          message: "File created!",
+        });
+      }
+    );
   });
 }
 
